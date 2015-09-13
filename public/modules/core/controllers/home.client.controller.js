9e3dd27cb747635fcabd275e2ccad328863cbfd7
Michael Stramel <Michael.Stramel@panerabread.com>
2015-09-12 19:38:37 -0500

    form validation crap

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,41 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+
+		$scope.citationSearchModel = {
+			firstName: null,
+			lastName: null,
+			dateOfBirth: null,
+			licenseNumber: null,
+			citationNumber: null
+		};
+
+		function isCitationSearchValid() {
+			return $scope.citationSearch.$dirty && $scope.citationSearch.$valid;
+		}
+
+		function isLicenseSearchValid() {
+			return $scope.licenseSearch.$dirty && $scope.licenseSearch.$valid;
+		}
+
+		function isNameSearchValid() {
+			return $scope.nameSearch.$dirty && $scope.nameSearch.$valid;
+		}
+
+		function hasFormBeenUsed() {
+			return !($scope.citationSearch.$pristine && $scope.licenseSearch.$pristine && $scope.nameSearch.$pristine);
+		}
+
+		$scope.isFormValid = function() {
+			return (
+				hasFormBeenUsed() && (
+					isCitationSearchValid() ||
+					isLicenseSearchValid() ||
+					isNameSearchValid()
+				)
+			);
+		}
+
 	}
 ]);
